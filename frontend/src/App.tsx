f9dd241689805e68a5d40bd125ee59133b1d869b
Sebastian Pacheco <sebastian575jpg@gmail.com>
2025-06-03 12:12:04 -0500

    feat: Eliminar archivos de configuración de paquetes innecesarios y actualizar dependencias en el frontend, incluyendo nuevos componentes y mejoras en la página de inicio de sesión.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Login" element={<Login />} />
-    </Routes>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
