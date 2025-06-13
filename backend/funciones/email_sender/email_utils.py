4a87985bfab29386be151ef26995b04d02083515
Sebastian Pacheco <sebastian575jpg@gmail.com>
2025-06-13 17:12:50 -0500

    feat: Implementar función para hashear contraseñas al insertar credenciales en el backend, asegurando que el email esté en minúsculas y mejorando la seguridad del almacenamiento de contraseñas.

---
+++ backend/funciones/email_sender/email_utils.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+
+def send_email(to_email: str, subject: str, message: str) -> bool:
+    try:
+        msg = MIMEMultipart()
+        msg["From"] = EMAIL_ADDRESS
+        msg["To"] = to_email
+        msg["Subject"] = subject
+
+        msg.attach(MIMEText(message, "plain"))
+
+        server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+        server.starttls()
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        server.sendmail(EMAIL_ADDRESS, to_email, msg.as_string())
+        server.quit()
+
+        return True
+    except Exception as e:
+        print("Error al enviar correo:", e)
+        return False
