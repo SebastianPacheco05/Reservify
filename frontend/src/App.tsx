dd2a3fc7d48edeb264411c89a1a36d02e40255a3
Sebastian Pacheco <sebastian575jpg@gmail.com>
2025-07-08 17:06:22 -0500

    refactor: actualizar la configuración de color base en components.json, agregar nuevo componente @radix-ui/react-select y actualizar versiones de dependencias en package.json y package-lock.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Insertar_cliente from "./pages/Insertar_cliente";
+import Registrar from "./pages/registrar";
 import Insertar_restaurante from "./pages/Insertar_restaurante";
 // el de credencial no lo veo necesario ya que en el cliente insertar se crea la credencial
 import Insertar_credencial from "./pages/Insertar_credencial";
@@ -10,8 +10,9 @@ import Editar_restaurante from "./pages/Editar_restaurante";
 import Borrar_restaurante from "./pages/Borrar_restaurante";
 import Borrar_cliente from "./pages/Borrar_cliente";
 import Dashboard from "./pages/Dashboard";
-import EmployeeDashboard from "./pages/employee-dashboard"
-
+import EmployeeDashboard from "./pages/employee-dashboard";
+import ClientDashboard from "./pages/Dashboard_cliente";
+import Restaurant from "./pages/Restaurant";
 
 import { ThemeProvider } from "./components/theme-provider";
 
@@ -21,15 +22,20 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
-        <Route path="/Insertar_cliente" element={<Insertar_cliente />} />
-        <Route path="/Insertar_restaurante" element={<Insertar_restaurante/>}/>
+        <Route path="/Registrar" element={<Registrar />} />
+        <Route
+          path="/Insertar_restaurante"
+          element={<Insertar_restaurante />}
+        />
         <Route path="/Insertar_credencial" element={<Insertar_credencial />} />
-        <Route path="/Editar_restaurante" element={<Editar_restaurante/>}/>
+        <Route path="/Editar_restaurante" element={<Editar_restaurante />} />
         <Route path="/Editar_cliente" element={<Editar_cliente />} />
-        <Route path="/Borrar_restaurante" element={<Borrar_restaurante/>}/>
+        <Route path="/Borrar_restaurante" element={<Borrar_restaurante />} />
         <Route path="/Borrar_cliente" element={<Borrar_cliente />} />
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/EmployeeDashboard" element={<EmployeeDashboard />} />
+        <Route path="/ClientDashboard" element={<ClientDashboard />} />
+        <Route path="/Restaurant" element={<Restaurant />} />
       </Routes>
     </ThemeProvider>
   );
